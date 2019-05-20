42af6ee2300197718a7e0f2ff8b7bcf162d9b7fa
cjfsu <kdtoaj14@ajou.ac.kr>
2019-05-20 19:29:47 +0900

    로그인 기능 구현

---
+++ app/src/main/java/com/mobitant/firebook/Login.java
@@ -0,0 +1,72 @@
+package com.mobitant.firebook;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.HashMap;
+
+public class Login extends AppCompatActivity implements ServerResponse{
+    Login main_this = this;
+    EditText editText;
+    EditText editText2;
+    Button button;
+    String sid;
+    String spw;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        ActionBar abr = getSupportActionBar();
+        abr.hide(); // Hide Action bar
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        editText = (EditText)findViewById(R.id.editText);
+        editText2 = (EditText)findViewById(R.id.editText2);
+        button = (Button)findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bt_login();
+            }
+        });
+    }
+
+    public void bt_login(){
+        sid = editText.getText().toString();
+        spw = editText2.getText().toString();
+        HashMap<String,String> user = new HashMap<>();
+        user.put("id",sid);
+        new Server().onDb("http://54.180.109.133:4000/logintest", user,  main_this);
+        if(spw.equals(0)){
+
+        }
+
+    }
+
+    @Override
+    public void processFinish(String output) {
+
+        try {
+            JSONArray jsonArray= new JSONArray(output);
+           if(jsonArray.getJSONObject(0).getString("pw").equals(spw)){
+               Toast.makeText(getApplicationContext(),"로그인 완료",Toast.LENGTH_LONG).show();
+               Intent intent = new Intent(Login.this,MainActivity.class);
+               startActivity(intent);
+           }
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
