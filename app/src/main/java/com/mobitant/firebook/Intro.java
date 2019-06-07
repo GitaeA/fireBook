2fe6f7096e55216fc560f9d69cb0d0f98f4c1776
cjfsu <kdtoaj14@ajou.ac.kr>
2019-06-07 17:58:44 +0900

    인트로 프로토 타입

---
+++ app/src/main/java/com/mobitant/firebook/Intro.java
@@ -0,0 +1,34 @@
+package com.mobitant.firebook;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+
+public class Intro extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        ActionBar abr = getSupportActionBar();
+        abr.hide(); // Hide Action bar
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intro);
+        startLoading();
+    }
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent intent = new Intent(Intro.this,Login.class);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+                finish();
+            }
+        }, 3100);
+
+
+    }
+}
+
