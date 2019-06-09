f831ea7542ba1efa11af9eb94190ff0955f80315
heypooo <jktmobile6637@gmail.com>
2019-06-10 03:24:32 +0900

    최종 레이아웃 수정

--- app/src/main/java/com/mobitant/firebook/Buy.java
+++ app/src/main/java/com/mobitant/firebook/Buy.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import org.json.JSONArray;
@@ -32,7 +33,7 @@ public class Buy extends Fragment implements ServerResponse, BuyRecyclerViewAdap
     LinearLayoutManager layoutManager;
     Buy buy = this;
     List<Books> booksList = new ArrayList<>();
-    Button buy_search_button;
+    ImageButton buy_search_button;
     String mSearch;
 
 
